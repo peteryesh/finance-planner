64150592bdea9dd3e2a7a9ca05725a561788055d
Sean Takafuji <seantakafuji@gmail.com>
2021-12-26 01:49:51 -0800

    run black on backend

--- backend/app.py
+++ backend/app.py
@@ -8,84 +8,90 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from sql_service import User, Account, Transaction
 
+
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    engine = create_engine('sqlite:///../../databases/finance_tracker.db')
+    engine = create_engine("sqlite:///../../databases/finance_tracker.db")
     Session = sessionmaker(bind=engine)
     metadata = MetaData()
 
     with Session() as session:
-        exists = session.query(User.username).filter_by(username='petercnoh').scalar() is not None
+        exists = (
+            session.query(User.username).filter_by(username="petercnoh").scalar()
+            is not None
+        )
         if exists:
-            session.query(User).filter_by(username='petercnoh').first()
+            session.query(User).filter_by(username="petercnoh").first()
         else:
-            new_user = User(username = 'petercnoh', first_name = 'peter', last_name = 'noh')
+            new_user = User(username="petercnoh", first_name="peter", last_name="noh")
             session.add(new_user)
         session.commit()
 
-    @app.route('/', methods=['GET'])
+    @app.route("/", methods=["GET"])
     def default():
-        return 'Hello world!'
+        return "Hello world!"
 
     ## User endpoints ##
-    @app.route('/adduser', methods=['POST'])
+    @app.route("/adduser", methods=["POST"])
     def add_user():
-        return ''
+        return ""
 
-    @app.route('/updateuser', methods=['POST'])
+    @app.route("/updateuser", methods=["POST"])
     def update_user():
-        return ''
+        return ""
 
-    @app.route('/deleteuser', methods=['GET'])
+    @app.route("/deleteuser", methods=["GET"])
     def delete_user():
-        return ''
+        return ""
 
-    @app.route('/viewuser', methods=['GET'])
+    @app.route("/viewuser", methods=["GET"])
     def view_user():
-        return ''
-    
+        return ""
+
     ## Transaction endpoints ##
-    @app.route('/addtransaction', methods=['POST'])
+    @app.route("/addtransaction", methods=["POST"])
     def add_transaction():
-        return ''
-    
-    @app.route('/updatetransaction', methods=['POST'])
+        return ""
+
+    @app.route("/updatetransaction", methods=["POST"])
     def update_transaction():
-        return ''
+        return ""
 
-    @app.route('/deletetransaction', methods=['GET'])
+    @app.route("/deletetransaction", methods=["GET"])
     def delete_transaction():
-        return ''
-    
-    @app.route('/viewtransactions', methods=['GET'])
-    def view_transactions():
-        return ''
-    
-    ## Account endpoints
-    @app.route('/addaccount', methods=['POST'])
-    def add_account():
-        return ''
-    
-    @app.route('/updateaccount', methods=['POST'])
-    def update_account():
-        return ''
-    
-    @app.route('/deleteaccount', methods=['GET'])
-    def delete_account():
-        return ''
+        return ""
 
-    @app.route('/viewaccount', methods=['GET'])
+    @app.route("/viewtransactions", methods=["GET"])
+    def view_transactions():
+        return ""
+
+    ## Account endpoints
+    @app.route("/addaccount", methods=["POST"])
+    def add_account():
+        return ""
+
+    @app.route("/updateaccount", methods=["POST"])
+    def update_account():
+        return ""
+
+    @app.route("/deleteaccount", methods=["GET"])
+    def delete_account():
+        return ""
+
+    @app.route("/viewaccount", methods=["GET"])
     def view_account():
-        return ''
+        return ""
 
     return app
 
+
 def main():
-    print('Starting app...')
+    print("Starting app...")
     app = create_app()
     app.run(debug=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
