85dd6d7517fe4ecb2b035d6052fa1ae6536dbe49
Peter Noh <petercnoh@gmail.com>
2021-12-26 02:20:39 -0800

    frontend garbo

--- finance-website/src/app/app.component.ts
+++ finance-website/src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+export interface User {
+    username: string;
+    first_name: string;
+    last_name: string;
+}
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'finance-website';
+    title = 'finance-website';
+
+    testUrl = 'http://localhost:5000';
+
+    constructor(private http: HttpClient) { }
+
+    submitUser() {
+        const new_user = {
+            username: 'petercnoh',
+            first_name: 'peter',
+            last_name: 'noh'
+        };
+        const response = this.http.post(`${this.testUrl}/adduser`, new_user);
+        response.subscribe(event => console.log(event));
+    }
 }
